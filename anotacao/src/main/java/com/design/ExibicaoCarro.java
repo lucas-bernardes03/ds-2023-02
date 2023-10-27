2451ca61ce30933de1b98fd2318a96628d670cfa
Lucas Bernardes <lucasbernardes24@gmail.com>
2023-10-27 16:30:03 -0300

    anotacao

---
+++ anotacao/src/main/java/com/design/ExibicaoCarro.java
@@ -0,0 +1,15 @@
+package com.design;
+
+@Observer(observado = "com.design.Carro")
+public class ExibicaoCarro implements ObservadorCarro {
+    private String nome;
+
+    public ExibicaoCarro(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void atualizar(Carro carro) {
+        System.out.println(nome + " - Modelo: " + carro.getModelo() + ", Velocidade: " + carro.getVelocidade() + " km/h");
+    }
+}
---
+++ anotacao/src/main/java/com/design/ObservadorCarro.java
@@ -0,0 +1,5 @@
+package com.design;
+
+public interface ObservadorCarro {
+    void atualizar(Carro carro);
+}
