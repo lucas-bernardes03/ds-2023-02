87b17b037d9b03e9da9b6186941af7e8abcba5e7
Lucas Bernardes <72103046+lucas-bernardes03@users.noreply.github.com>
2023-10-30 21:28:03 -0300

    Delete decorator directory

--- decorator/src/main/java/com/design/App.java
+++
@@ -1,8 +0,0 @@
-package com.design;
-
-public class App {
-    public static void main(String[] args) {
-        Expressao e = new Soma(new Constante(5),new Multiplica(new Constante(2), new Constante(3)));
-        System.out.println("Resultado: " + e.valor());
-    }
-}
